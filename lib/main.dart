1a5a95ff1e7b9c03f35bbabe20f51cdd078cf483
Lichyo <lichyo003@gmail.com>
2024-02-01 00:10:22 +0800

    create homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
-void main()=>runApp(Waves());
+import 'view/home_page.dart';
+
+void main() => runApp(Waves());
+
 class Waves extends StatelessWidget {
   const Waves({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark(),
-      initialRoute: ,
-      routes:{
-
-      } ,
+      // theme: ThemeData.dark(),
+      // initialRoute:,
+      // routes: {
+      // },
+      home: HomePage(),
     );
   }
 }
