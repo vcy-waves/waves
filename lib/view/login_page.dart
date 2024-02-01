7c5edba2b0e242bf9fd138c0dd4c67c0d45330d7
Yitong Chen <ttcyt1029@gmail.com>
2024-02-01 21:56:10 +0800

    auth error

--- lib/view/login_page.dart
+++ lib/view/login_page.dart
@@ -1,45 +1,80 @@
 import 'package:flutter/material.dart';
-class LoginPage extends StatelessWidget {
+import 'package:firebase_auth/firebase_auth.dart';
+
+class LoginPage extends StatefulWidget {
   static String id = 'login_page';
+
   const LoginPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.symmetric(vertical: 20,horizontal: 20),
-        child: Column(
-          children: [
-            InputField(textInField: 'Name',),
-            InputField(textInField: 'E-mail',),
-          ],
-        ),
-      ),
-    );
-  }
+  State<LoginPage> createState() => _LoginPageState();
 }
 
-class InputField extends StatelessWidget {
-  String textInField;
-  InputField({
-    required this.textInField,
-    super.key,
-  });
-
+class _LoginPageState extends State<LoginPage> {
+  final _auth = FirebaseAuth.instance;
+  String email = '';
+  String password = '';
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      decoration: InputDecoration(
-        labelText:textInField,
-        enabledBorder:OutlineInputBorder(
-          borderRadius: BorderRadius.circular(20),
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(20),
-        ),
-        filled: true,
-        fillColor: Colors.grey,
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(
+                  onChanged: (value) {
+                    email = value;
+                  },
+                  keyboardType: TextInputType.emailAddress,
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                    hintText: 'enter your email',
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                TextField(
+                  onChanged: (value) {
+                    password = value;
+                  },
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                    hintText: 'enter your password',
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                  ),
+                ),
+                TextButton(
+                  onPressed: ()async{
+                    try{
+                      final user = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+                      print(user.credential!.accessToken);
+                    }catch(e){
+                      print(e);
+                    }
+                  },
+                  child: Text('login'),
+                )
+              ],
+                        ),
+            ),),
 
+        ],
       ),
     );
   }
