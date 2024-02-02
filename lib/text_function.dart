6a1227145ce7790406241149bfee02eda96e8390
Yitong Chen <ttcyt1029@gmail.com>
2024-02-02 11:37:56 +0800

    login and registrataion

--- lib/text_function.dart
+++ lib/text_function.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:waves/view/login_page.dart';
+import 'package:waves/view/registration_page.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
--- lib/view/login_page.dart
+++ lib/view/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:waves/view/home_page.dart';
 
 class LoginPage extends StatefulWidget {
   static String id = 'login_page';
@@ -14,6 +15,7 @@ class _LoginPageState extends State<LoginPage> {
   final _auth = FirebaseAuth.instance;
   String email = '';
   String password = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,56 +26,63 @@ class _LoginPageState extends State<LoginPage> {
               padding: EdgeInsets.symmetric(horizontal: 15),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextField(
-                  onChanged: (value) {
-                    email = value;
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                  textAlign: TextAlign.center,
-                  decoration: InputDecoration(
-                    hintText: 'enter your email',
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(50),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(50),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                TextField(
-                  onChanged: (value) {
-                    password = value;
-                  },
-                  textAlign: TextAlign.center,
-                  decoration: InputDecoration(
-                    hintText: 'enter your password',
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(50),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(50),
+                children: [
+                  TextField(
+                    onChanged: (value) {
+                      email = value;
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                      hintText: 'enter your email',
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(50),
+                      ),
                     ),
                   ),
-                ),
-                TextButton(
-                  onPressed: ()async{
-                    try{
-                      final user = await _auth.createUserWithEmailAndPassword(email: email, password: password);
-                      print(user.credential!.accessToken);
-                    }catch(e){
-                      print(e);
-                    }
-                  },
-                  child: Text('login'),
-                )
-              ],
-                        ),
-            ),),
-
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  TextField(
+                    onChanged: (value) {
+                      password = value;
+                    },
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                      hintText: 'enter your password',
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  TextButton(
+                    onPressed: () async {
+                      try {
+                        final user = await _auth.signInWithEmailAndPassword(
+                            email: email, password: password);
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => HomePage()));
+                      } catch (e) {
+                        print(e);
+                      }
+                    },
+                    child: Text('login'),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
