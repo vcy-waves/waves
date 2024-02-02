f19806761a7dd9292409785015190a3068a10726
LI,CHI-YU <111233017+Lichyo@users.noreply.github.com>
2024-02-02 14:40:51 +0800

    promote event & push notification (#10)
    * setup app icon
    
    * noti feature ios setup finished
    
    * sending email for user function finished
    
    * create noti template
    
    * testing : integrate noti template with ios
    
    * create homepage
    
    * testing : geolocator works
    
    * testing : get full address with google service
    
    * checkout to fix firebase issue
    
    * broadcasting function finished
    
    * upload covers and setup basic homepage

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,3 +1,4 @@
+import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -10,12 +11,15 @@ class _HomePageState extends State<HomePage> {
   // const HomePage({super.key});
   String? location;
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: Stack(
         children: [
+          Image.asset(
+            'images/ocean/cover_1.jpg',
+            opacity: const AlwaysStoppedAnimation(.5),
+          ),
           Padding(
             padding: const EdgeInsets.only(
               top: 50.0,
@@ -23,12 +27,44 @@ class _HomePageState extends State<HomePage> {
               right: 20.0,
               bottom: 20.0,
             ),
-            child: Center(
+            child: Align(
+              alignment: Alignment.topCenter,
               child: Text(
                 location ?? 'There is where you are',
-                style: const TextStyle(fontFamily: 'Playpen_Sans', fontSize: 20.0),
+                style:
+                    const TextStyle(fontFamily: 'Playpen_Sans', fontSize: 20.0),
               ),
             ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              const Expanded(
+                child: SizedBox(),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade200,
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  ),
+                ),
+                height: 370,
+                width: double.infinity,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      children: [],
+                    ),
+                    Row(
+                      children: [],
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ],
       ),
