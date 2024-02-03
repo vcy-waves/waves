74522abe8b6ae747f85012a411351e1de0b8b32d
Lichyo <lichyo003@gmail.com>
2024-02-03 11:23:21 +0800

    homepage set up

--- lib/lab.dart
+++ lib/lab.dart
@@ -3,7 +3,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:waves/firebase_options.dart';
 import 'package:waves/view/home_page.dart';
-import 'package:flutter/material.dart';
 import 'services/notification.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_email_sender/flutter_email_sender.dart';
@@ -14,9 +13,13 @@ import 'package:http/http.dart' as http;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(MaterialApp(
-    home: HomePage(),
-  ));
+  runApp(
+    MaterialApp(
+      home: HomePage(),
+      // home: Lab(),
+    ),
+  );
+}
 
 class Lab extends StatefulWidget {
   const Lab({super.key});
@@ -51,6 +54,9 @@ class _LabState extends State<Lab> {
                     notiType: NotiType.fine,
                     location: 'New Taipei City',
                     initiator: 'Chi-Yu',
+                    flutterLocalNotificationsPlugin:
+                        flutterLocalNotificationsPlugin,
+                  );
                 },
               ),
               TextButton(
--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,15 +1,26 @@
-import 'dart:ffi';
+import 'package:waves/services/notification.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:waves/components/tool_box.dart';
 
 class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  // const HomePage({super.key});
   String? location;
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  @override
+  void initState() {
+    super.initState();
+    NotificationService.getNotificationOnFirebase(
+        flutterLocalNotificationsPlugin);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,35 +47,92 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
           ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              const Expanded(
-                child: SizedBox(),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.grey.shade200,
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(30),
-                    topRight: Radius.circular(30),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                const Expanded(
+                  child: SizedBox(),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade200,
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
+                    ),
+                  ),
+                  height: 370,
+                  width: double.infinity,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Padding(
+                        padding:
+                            EdgeInsets.only(top: 30, left: 20.0, bottom: 25.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Hi Chi-Yu',
+                              textAlign: TextAlign.left,
+                              style: TextStyle(
+                                fontFamily: 'Playpen_Sans',
+                                fontSize: 25.0,
+                              ),
+                            ),
+                            Text(
+                              'You have been clean up 3 times',
+                              style: TextStyle(
+                                fontFamily: 'Playpen_Sans',
+                                fontSize: 18.0,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          ToolBox(
+                            color: Colors.blueGrey.shade300,
+                            title: 'Clean Up',
+                            icon: Icons.opacity,
+                            iconColor: Colors.blueAccent.shade200,
+                            onTap: () {},
+                          ),
+                          ToolBox(
+                            color: Colors.blueGrey.shade300,
+                            title: 'Hold Event',
+                            icon: Icons.library_books_rounded,
+                            iconColor: Colors.amber.shade500,
+                            onTap: () {},
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          ToolBox(
+                            color: Colors.blueGrey.shade300,
+                            title: 'Waves Rank',
+                            icon: Icons.star_outline_rounded,
+                            iconColor: Colors.deepOrangeAccent.shade200,
+                            onTap: () {},
+                          ),
+                          ToolBox(
+                            color: Colors.blueGrey.shade300,
+                            title: 'Needs Help',
+                            icon: Icons.help_rounded,
+                            iconColor: Colors.green.shade300,
+                            onTap: () {},
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
                 ),
-                height: 370,
-                width: double.infinity,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Row(
-                      children: [],
-                    ),
-                    Row(
-                      children: [],
-                    ),
-                  ],
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ],
       ),
