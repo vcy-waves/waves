17d6fd80e2999bec890c18d1e32e434b693e645b
Yitong Chen <ttcyt1029@gmail.com>
2024-02-03 13:33:38 +0800

    add background to login page and registraction page

--- lib/view/registration_page.dart
+++ lib/view/registration_page.dart
@@ -22,145 +22,158 @@ class _RegistrationPageState extends State<RegistrationPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: Stack(
         children: [
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextField(
-                    onChanged: (value) {
-                      name = value;
-                    },
-                    keyboardType: TextInputType.emailAddress,
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                      hintText: 'enter your name',
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(50),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(50),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  TextField(
-                    onChanged: (value) {
-                      email = value;
-                    },
-                    keyboardType: TextInputType.emailAddress,
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                      hintText: 'enter your email',
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(50),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(50),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  TextField(
-                    onChanged: (value) {
-                      password = value;
-                    },
-                    obscureText: true,
-                    keyboardType: TextInputType.emailAddress,
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                      hintText: 'enter your password',
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(50),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(50),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  TextField(
-                    onChanged: (value) {
-                      location = value;
-                    },
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                      hintText: 'enter your location',
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(50),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(50),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  Row(
+          Image.asset(
+            'images/ocean/cover_6.JPG',
+            opacity: const AlwaysStoppedAnimation(.5),
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            fit: BoxFit.cover,
+          ),
+          Column(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Checkbox(
-                        checkColor: Colors.white,
-                        value: isChecked,
-                        hoverColor: Colors.grey,
-                        focusColor: Colors.blue,
-                        onChanged: (bool? value) {
-                          setState(() {});
-                          isChecked = value!;
+                      TextField(
+                        onChanged: (value) {
+                          name = value;
                         },
+                        keyboardType: TextInputType.emailAddress,
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration(
+                          hintText: 'enter your name',
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                        ),
                       ),
-                      Text(
-                        'allow to receive email from waves',
-                        style: TextStyle(
-                          fontSize: 17,
-                          color: Colors.grey.shade700,
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      TextField(
+                        onChanged: (value) {
+                          email = value;
+                        },
+                        keyboardType: TextInputType.emailAddress,
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration(
+                          hintText: 'enter your email',
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      TextField(
+                        onChanged: (value) {
+                          password = value;
+                        },
+                        obscureText: true,
+                        keyboardType: TextInputType.emailAddress,
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration(
+                          hintText: 'enter your password',
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      TextField(
+                        onChanged: (value) {
+                          location = value;
+                        },
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration(
+                          hintText: 'enter your address',
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Checkbox(
+                            checkColor: Colors.white,
+                            value: isChecked,
+                            hoverColor: Colors.grey,
+                            focusColor: Colors.blue,
+                            activeColor: Colors.blue,
+                            onChanged: (bool? value) {
+                              setState(() {});
+                              isChecked = value!;
+                            },
+                          ),
+                          Text(
+                            'allow to receive email from waves',
+                            style: TextStyle(
+                              color: Colors.black54,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Material(
+                        color: Colors.transparent,
+                        borderRadius: BorderRadius.circular(50),
+                        // shadowColor: Colors.white,
+                        // elevation: 3,
+                        child: MaterialButton(
+                          minWidth: MediaQuery.of(context).size.width * 1,
+                          height: 55,
+                          onPressed: () {
+                            try {
+                              final user = _auth.createUserWithEmailAndPassword(
+                                  email: email, password: password);
+                              final data = _firestore.collection('users').add({
+                                'name': name,
+                                'email': email,
+                                'address': location,
+                                'broadcasting': isChecked,
+                              });
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => HomePage()));
+                            } catch (e) {
+                              print(e);
+                            }
+                          },
+                          child: Text(
+                            'registration',
+                            style: TextStyle(color: Colors.black54),
+                          ),
                         ),
                       ),
                     ],
                   ),
-                  Material(
-                    color: Color(0xFF86B6F6),
-                    borderRadius: BorderRadius.circular(50),
-                    shadowColor: Colors.blue.shade500,
-                    elevation: 3,
-                    child: MaterialButton(
-                      minWidth: MediaQuery.of(context).size.width*1,
-                      height: 55,
-                      onPressed: (){
-                        try {
-                          final user = _auth.createUserWithEmailAndPassword(
-                              email: email, password: password);
-                          final data = _firestore.collection('users').add({
-                            'name': name,
-                            'email': email,
-                            'location': location,
-                            'broadcasting': isChecked,
-                          });
-                          Navigator.push(context, MaterialPageRoute(builder:(context)=>HomePage()));
-                        } catch (e) {
-                          print(e);
-                        }
-                      },
-                      child: Text(
-                        'registration',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                  ),
-
-                ],
+                ),
               ),
-            ),
+            ],
           ),
         ],
       ),
