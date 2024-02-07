fc15ff8e7326912416b047f14d3937fe13ef288e
Lichyo <lichyo003@gmail.com>
2024-02-07 19:19:13 +0800

    test for new branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:waves/firebase_options.dart';
-import 'package:waves/view/home_page.dart';
 import 'package:waves/view/login_page.dart';
 
 void main() async {
