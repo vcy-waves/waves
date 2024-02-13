f89809eb12eea237fde9bff98b10500df1b4f5cd
Yitong Chen <ttcyt1029@gmail.com>
2024-02-13 20:26:22 +0800

    profile page isn't finished

--- lib/view/profile_page.dart
+++ lib/view/profile_page.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:waves/services/users.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
