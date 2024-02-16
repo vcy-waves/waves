318c285e73950e57ff64084e4f9fc40bbd508842
LI,CHI-YU <111233017+Lichyo@users.noreply.github.com>
2024-02-16 16:01:22 +0800

    set up post model & service (#29)
    * waves
    
    * create post service and model

---
+++ lib/model/post.dart
@@ -0,0 +1,17 @@
+import 'package:image_picker/image_picker.dart';
+
+class Post {
+  final String location;
+  final DateTime lastUpdate;
+  XFile? image;
+  final String initiator;
+  final int id;
+
+  Post({
+    required this.location,
+    required this.initiator,
+    required this.lastUpdate,
+    this.image,
+    required this.id,
+  });
+}
