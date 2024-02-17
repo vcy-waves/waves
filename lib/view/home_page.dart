00476388dd54d62a13973d5b5e8e8fe39a6a1505
Lichyo <lichyo003@gmail.com>
2024-02-18 00:11:24 +0800

    service doesn't work

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -9,6 +9,9 @@ import 'package:url_launcher/url_launcher.dart';
 import 'package:waves/view/profile_page.dart';
 import 'dart:io' show Platform;
 import 'package:waves/view/post_event_page.dart';
+import 'package:waves/constants.dart';
+import 'package:flutter_background_service/flutter_background_service.dart';
+import 'dart:ui';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -17,6 +20,7 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
+
 class _HomePageState extends State<HomePage> {
   final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
       FlutterLocalNotificationsPlugin();
@@ -24,6 +28,7 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
+    initService();
     if (Platform.isAndroid) {
       flutterLocalNotificationsPlugin
           .resolvePlatformSpecificImplementation<
@@ -34,6 +39,12 @@ class _HomePageState extends State<HomePage> {
     NotificationService.getNotificationOnFirebase(
       flutterLocalNotificationsPlugin,
     );
+    update();
+  }
+
+  Future<void> update() async {
+    await AccountService.fetchAccount();
+    setState(() {});
   }
 
   @override
@@ -133,8 +144,15 @@ class _HomePageState extends State<HomePage> {
                             icon: Icons.library_books_rounded,
                             iconColor: Colors.amber.shade500,
                             onTap: () async {
-                              Navigator.of(context).push(MaterialPageRoute(
-                                  builder: (context) => const PostEventPage()));
+                              // Navigator.of(context).push(MaterialPageRoute(
+                              //     builder: (context) => const PostEventPage()));
+                              await NotificationService.promoteEvent(
+                                notiType: NotiType.fine,
+                                initiator: 'chiyu',
+                                location: 'location',
+                                flutterLocalNotificationsPlugin:
+                                    flutterLocalNotificationsPlugin,
+                              );
                             },
                           ),
                         ],
